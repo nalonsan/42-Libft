b34b190e90eaced7858998785c567b9cffeebb51
nalonsan <55792949+nalonsan@users.noreply.github.com>
2019-11-29 18:08:59 +0100

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -2,24 +2,14 @@
 
 size_t    ft_strlcpy(char *dst, const char *src, size_t len_dst)
 {
-  unsigned char *d;
-  unsigned char *s;
-  size_t        n;
-  
-  d = (unsigned char *)dst;
-  s = (unsigned char *)src;
-  n = size_t len_dst;
-  while (n < len_dst)
-  {
-    n++;
-    if((*dst = *src) == '\0')
-      break ;
-  }
-  if (n < len_dst)
-    return (n);
-  if (n > 0)
-    *(dst - 1) = '\0';
-  return (n + strlen (src));
+  i = 0;
+	while (src[i] != '\0' && i < (size - 1))
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (strlen(src));
 }
 
 #include <string.h>
